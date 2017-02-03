df1fc281c316a1e751cb7bfd8d07d378fdfe2385
Mark Mandel <markmandel@google.com>
2017-02-03 01:52:34 -0800

    Fixes for GDC

--- script.js
+++ script.js
@@ -50,9 +50,18 @@ var insertByName = function(index, value) {
 };
 
 var groupByName = function() {
-	$.each(pods.items, insertByName);
-	$.each(controllers.items, insertByName);
-	$.each(services.items, insertByName);
+	if(pods.items) {
+		$.each(pods.items, insertByName);	
+	}
+
+	if(controllers.items) {
+		$.each(controllers.items, insertByName);
+	}
+
+	if(services.items) {
+		$.each(services.items, insertByName);
+	}
+
 };
 
 var matchesLabelQuery = function(labels, selector) {
@@ -67,42 +76,48 @@ var matchesLabelQuery = function(labels, selector) {
 
 var connectControllers = function() {
     connectUses();
-	for (var i = 0; i < controllers.items.length; i++) {
-		var controller = controllers.items[i];
-    //console.log("controller: " + controller.metadata.name)
-		for (var j = 0; j < pods.items.length; j++) {
-			var pod = pods.items[j];
-			if (pod.metadata.labels.name == controller.metadata.labels.name) {
-        if (controller.metadata.labels.version && pod.metadata.labels.version && (controller.metadata.labels.version != pod.metadata.labels.version)) {
-          continue;
-        }
-        //console.log('connect controller: ' + 'controller-' + controller.metadata.name + ' to pod-' + pod.metadata.name);
-				jsPlumb.connect({
-					source: 'controller-' + controller.metadata.name,
-					target: 'pod-' + pod.metadata.name,
-					anchors:["Bottom", "Bottom"],
-					paintStyle:{lineWidth:5,strokeStyle:'rgb(51,105,232)'},
-					joinStyle:"round",
-					endpointStyle:{ fillStyle: 'rgb(51,105,232)', radius: 7 },
-					connector: ["Flowchart", { cornerRadius:5 }]});
+
+	if(controllers.items) {
+		for (var i = 0; i < controllers.items.length; i++) {
+			var controller = controllers.items[i];
+		  //console.log("controller: " + controller.metadata.name)
+			for (var j = 0; j < pods.items.length; j++) {
+				var pod = pods.items[j];
+				if (pod.metadata.labels.name == controller.metadata.labels.name) {
+		      if (controller.metadata.labels.version && pod.metadata.labels.version && (controller.metadata.labels.version != pod.metadata.labels.version)) {
+		        continue;
+		      }
+		      //console.log('connect controller: ' + 'controller-' + controller.metadata.name + ' to pod-' + pod.metadata.name);
+					jsPlumb.connect({
+						source: 'controller-' + controller.metadata.name,
+						target: 'pod-' + pod.metadata.name,
+						anchors:["Bottom", "Bottom"],
+						paintStyle:{lineWidth:5,strokeStyle:'rgb(51,105,232)'},
+						joinStyle:"round",
+						endpointStyle:{ fillStyle: 'rgb(51,105,232)', radius: 7 },
+						connector: ["Flowchart", { cornerRadius:5 }]});
+				}
 			}
 		}
 	}
-	for (var i = 0; i < services.items.length; i++) {
-		var service = services.items[i];
-    //            if (service.metadata.name == 'kubernetes' || service.metadata.name == 'skydns' || service.metadata.name == 'kubernetes-ro') { continue; }
-		for (var j = 0; j < pods.items.length; j++) {
-			var pod = pods.items[j];
-      //console.log('connect service: ' + 'service-' + service.metadata.name + ' to pod-' + pod.metadata.name);
-			if (matchesLabelQuery(pod.metadata.labels, service.spec.selector)) {
-				jsPlumb.connect(
-					{
-						source: 'service-' + service.metadata.name,
-						target: 'pod-' + pod.metadata.name,
-						anchors:["Bottom", "Top"],
-						paintStyle:{lineWidth:5,strokeStyle:'rgb(0,153,57)'},
-						endpointStyle:{ fillStyle: 'rgb(0,153,57)', radius: 7 },
-						connector:["Flowchart", { cornerRadius:5 }]});
+
+	if(services.items) {
+		for (var i = 0; i < services.items.length; i++) {
+			var service = services.items[i];
+		  //            if (service.metadata.name == 'kubernetes' || service.metadata.name == 'skydns' || service.metadata.name == 'kubernetes-ro') { continue; }
+			for (var j = 0; j < pods.items.length; j++) {
+				var pod = pods.items[j];
+		    //console.log('connect service: ' + 'service-' + service.metadata.name + ' to pod-' + pod.metadata.name);
+				if (matchesLabelQuery(pod.metadata.labels, service.spec.selector)) {
+					jsPlumb.connect(
+						{
+							source: 'service-' + service.metadata.name,
+							target: 'pod-' + pod.metadata.name,
+							anchors:["Bottom", "Top"],
+							paintStyle:{lineWidth:5,strokeStyle:'rgb(0,153,57)'},
+							endpointStyle:{ fillStyle: 'rgb(0,153,57)', radius: 7 },
+							connector:["Flowchart", { cornerRadius:5 }]});
+				}
 			}
 		}
 	}
@@ -307,6 +322,9 @@ var loadData = function() {
 
 	var req2 = $.getJSON("/api/v1/replicationcontrollers?labelSelector=visualize%3Dtrue", function( data ) {
 		controllers = data;
+		if(!data.items) {
+			return
+		}
 		$.each(data.items, function(key, val) {
       val.type = 'replicationController';
       //console.log("Controller ID = " + val.metadata.name)
@@ -316,8 +334,9 @@ var loadData = function() {
 
 	var req3 = $.getJSON("/api/v1/services?labelSelector=visualize%3Dtrue", function( data ) {
 		services = data;
-		//console.log("loadData(): Services");
-		//console.log(services);
+		if(!data.items) {
+			return
+		}
 		$.each(data.items, function(key, val) {
       val.type = 'service';
       //console.log("service ID = " + val.metadata.name)
@@ -326,8 +345,9 @@ var loadData = function() {
 
 	var req4 = $.getJSON("/api/v1/nodes", function( data ) {
 		nodes = data;
-		//console.log("loadData(): Services");
-		//console.log(nodes);
+		if(!data.items) {
+			return
+		}
 		$.each(data.items, function(key, val) {
       val.type = 'node';
       //console.log("service ID = " + val.metadata.name)
